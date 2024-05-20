fcf9f99e24f4139e714839f763cbc27270631768
Venkat Katru <158803424+Venkat-Katru@users.noreply.github.com>
2024-05-20 10:05:46 +0530

    Add files via upload

---
+++ CRUDForms/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CRUDForms
+{
+    public partial class Form1 : Form
+    {
+        List<TaskItem> taskList = new List<TaskItem>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "ID",
+                DataPropertyName = "id",
+                HeaderText = "ID"
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "Task",
+                DataPropertyName = "task",
+                HeaderText = "Task"
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "Description",
+                DataPropertyName = "description",
+                HeaderText = "Description"
+            });
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(textBox1.Text);
+            string task = textBox2.Text;
+            string description = textBox3.Text;
+
+            TaskItem t = new TaskItem(id, task, description);
+            taskList.Add(t);
+            MessageBox.Show("Added Succesfully");
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(textBox1.Text);
+            string task = textBox2.Text;
+            string description = textBox3.Text;
+
+            
+            TaskItem taskToUpdate = taskList.Find(t => t.id == id);
+
+            if (taskToUpdate != null)
+            {
+                
+                taskToUpdate.task = task;
+                taskToUpdate.description = description;
+
+                
+                MessageBox.Show("Task updated successfully.");
+            }
+            else
+            {
+                
+                MessageBox.Show("Task not found.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(textBox1.Text);
+
+            
+            TaskItem taskToDelete = taskList.Find(t => t.id == id);
+
+            if (taskToDelete != null)
+            {
+                
+                taskList.Remove(taskToDelete);
+
+                
+                MessageBox.Show("Task deleted successfully.");
+            }
+            else
+            {
+                
+                MessageBox.Show("Task not found.");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = taskList;
+        }
+    }
+}
---
+++ CRUDForms/Tasks.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDForms
+{
+    public class TaskItem
+    {
+        private int Id;
+        private string Task;
+        private string Description;
+
+
+        public TaskItem() { }
+
+        public TaskItem(int id, string task, string description)
+        {
+            this.Id = id;
+            this.Task = task;
+            this.Description = description;
+        }
+
+        public int id
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+
+        public string task
+        {
+
+            get { return Task; }
+            set { Task = value; }
+        }
+
+        public string description
+        {
+            get { return Description; }
+            set { Description = value; }
+
+        }
+    }
+}
